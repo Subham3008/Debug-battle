908f0430f5390837d9e6bc15331e8a29d5b9ba7c
Subham3008 <subhamsamanta065@gmail.com>
2026-05-03 17:47:22 +0530

    fix: correct Navbar search input import path

--- 04-spotify/src/features/dashboard/ui/components/Navbar.jsx
+++ 04-spotify/src/features/dashboard/ui/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import SearchInput from "../../../search/ui/components/searchInput";
 import { House, LogIn } from "lucide-react";
 import { useDashboard } from "../../hooks/useDashboard";
+import SearchInput from "@/features/search/ui/components/SearchInput";
 
 const Navbar = () => {
   const { navigate } = useDashboard();
@@ -9,7 +9,10 @@ const Navbar = () => {
     <div className="h-[10%] bg-black text-white px-10 py-5 flex justify-between items-center">
       <h1>Spotify</h1>
       <div className="flex gap-6 items-center">
-        <House className="cursor-pointer" onClick={() => navigate("/dashboard")} />
+        <House
+          className="cursor-pointer"
+          onClick={() => navigate("/dashboard")}
+        />
         <div>
           <SearchInput placeholder="Search for a song" type="text" />
         </div>
