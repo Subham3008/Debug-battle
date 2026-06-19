5103ec7f870295725eac04c3bac27fe85d99ef19
Subham3008 <subhamsamanta065@gmail.com>
2026-06-19 23:58:32 +0530

    install packages and config server

---
+++ 08-linkTree/server/src/config/env.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv"
+dotenv.config()
+import z from "zod"
+import appConstant from "../shared/constant/app.constant.js"
+
+
+const envSchema = z.object({
+  PORT: z.coerce.number().default(appConstant.PORT),
+  MONGO_URI: z.string().default(appConstant.MONGO_URI),
+  NODE_ENV: z.string().default(appConstant.NODE_ENV)
+})
+
+const parsed = envSchema.safeParse(process.env)
+
+if (!parsed.success) {
+  console.log("Check your env's");
+  process.exit(1)
+}
+
+export default parsed.data
---
+++ 08-linkTree/server/src/shared/constant/app.constant.js
@@ -0,0 +1,5 @@
+export default {
+  PORT: 5000,
+  NODE_ENV: "development",
+  MONGO_URI: "mongodb://localhost:27017/linktree"
+}
